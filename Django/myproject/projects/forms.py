1f1a707a069475fff996543181b14cec77f38e93
AbrarZaved <abrarzaved2002@gmail.com>
2024-01-19 21:45:13 +0600

    delete

--- Django/myproject/projects/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import CLUB
-
-class ProjectForm(ModelForm):
-    class Meta:
-        model = CLUB
-        fields = ['name','Student_ID','Student_Mail','Phone_Number','Preferred_Club',
-                  'tags','Reason_behind_joinning_club','Image',]
--- New folder/weather/weathermonitor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WeathermonitorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weathermonitor'
